2bc352cec9ab8ec04ed57b13032142ba295214ec
Mohammed Saiful Islam <mohammedislam01865@gmail.com>
2026-04-20 16:58:54 +0600

    sec-2 run

---
+++ 02-infinity-scroll/script.js
@@ -0,0 +1,55 @@
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+let photosArray = [];
+
+
+
+// Helper Function to Set Attributes on DOM Elements
+function setAttributes(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
+
+// 
+function displayPhotos() {
+    photosArray.forEach((photo) => {
+        // Create <a> to link to Unsplash
+        const item = document.createElement('a');
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank',
+        });
+        // Create <img> for photo
+        const img = document.createElement('img');
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        });
+        // Put <img> inside <a>, then put both inside imageContainer Element
+        item.appendChild(img);
+        imageContainer.appendChild(item);
+    });
+}
+
+// Unsplash API
+const count = 10;
+const apiKey = '59XIik_cdddNnpKYX_Ix7zeDOWun9K5POOpCL7vPUOA';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+
+async function getPhotos() {
+    try {
+        const response = await fetch(apiUrl);
+        photosArray = await response.json();
+        displayPhotos();
+        // console.log(photosArray);
+    } catch (error) {
+
+    }
+}
+
+getPhotos();
