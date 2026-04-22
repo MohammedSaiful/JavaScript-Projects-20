08440f53f0b0bbd69cfe501b3460c7050c3fbe21
Mohammed Saiful Islam <mohammedislam01865@gmail.com>
2026-04-22 12:20:16 +0600

    sec-5 js

---
+++ 03-Picture-In-Picture/script.js
@@ -0,0 +1,29 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+
+
+async function selectMediaStream() {
+    try {
+        const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = mediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    } catch (error) {
+        console.log('error is here:', error)
+    }
+
+}
+
+button.addEventListener('click', async () => {
+    // disable button
+    button.disabled = true;
+    // start picture in picture
+    await videoElement.requestPictureInPicture();
+    // reset button
+    button.disabled = false;
+});
+
+// func call
+selectMediaStream();
